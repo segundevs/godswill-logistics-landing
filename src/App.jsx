0ba2bbb3e11e9c2bbfe606fc5c13782f054c6b23
segundevs <tushbash@gmail.com>
2022-05-25 03:51:51 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Process from './components/Process';
+import FAQS from './components/FAQS';
+import Action from './components/Action';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <Header />
+        <main className="container">
+          <Hero />
+          <Services />
+          <Process />
+          <FAQS />
+          <Action />
+          <Contact />
+        </main>
+      <Footer />
+    </React.Fragment>
+  )
+}
+
+export default App
