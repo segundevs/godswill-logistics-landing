899d83adfe980cdef5c2541e01d50d058dbb90ea
segundevs <tushbash@gmail.com>
2022-05-28 06:43:42 -0700

    updated hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import playstore from '../assets/playstore.png';
+import playstore from '../assets/playstore.svg';
 import mobileImg from '../assets/mobile-img.jpg';
 import desktopImg from '../assets/desktop-img.png';
 
@@ -25,15 +25,15 @@ const Hero = () => {
 
 
   return (
-    <section className='hero'>
+    <section className='hero' id='home'>
       <div className="hero_message">
         <span className='hero_message-sec'>Quality Service</span>
         <h1>The <span className='colored-text'>faster <br /> than light</span> delivery <br />service</h1>
         <p>An ideal way to deliver your goods and <br /> packages</p>
         
-        <button>
+        <a href="#" target="_blank">
           <img src={playstore} alt="playstore-link" />
-        </button>
+        </a>
       </div>
       <div className="hero_img">
         <img src={dimensions.width < 960 ? mobileImg : desktopImg} alt="hero" />
