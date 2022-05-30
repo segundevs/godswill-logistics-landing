5219f5f90880279fc713eb8dc18a63bd230d5ca6
segundevs <tushbash@gmail.com>
2022-05-30 08:47:23 -0700

    added accordion

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Accordion = ({ title, content }) => {
+
+  const [isActive, setIsActive] = React.useState(false);
+
+  return (
+    <div className={isActive ? "accordion active-color" : "accordion"}>
+      <div className="accordion_title" onClick={() => setIsActive(!isActive)}>
+        <div className={isActive && "is_active"}>{title}</div>
+        <div className={isActive ? "is_active icon" : "icon"}>{isActive ? '-' : '+'}</div>
+      </div>
+      {isActive && <div className="accordion_content">{content}</div>}
+    </div>
+  );
+};
+
+export default Accordion
