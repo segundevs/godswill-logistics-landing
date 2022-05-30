a8e8ab66758f11f27e96a0e638a691cf2801a042
segundevs <tushbash@gmail.com>
2022-05-30 08:48:14 -0700

    updated faqs component

--- src/components/FAQS.jsx
+++ src/components/FAQS.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import Faq from "react-faq-component";
-
-
+import React from 'react';
+import Accordion from './Accordion';
 
 const FAQS = () => {
 
-  const data = {
-    rows: [
+  const data = [
         {
             title: "How do I pay for delivery",
             content: "We provide a very comfortable payment method",
@@ -27,33 +24,14 @@ const FAQS = () => {
             title: "How do I guarantee the safety of my package",
             content: "We are insured by a leading insurance company in Nigeria, so your package is safe with us",
         },
-    ],
-};
-
-
-const styles = {
-    bgColor: 'white',
-    titleTextColor: "hsl(0, 0%, 20%)",
-    rowTitleColor: "hsl(0, 0%, 20%)",
-    rowContentColor: 'hsl(0, 0%, 20%)',
-    arrowColor: "hsl(259,1%,48%)",
-};
-
-const config = {
-  animate: true,
-  expandIcon: "+",
-  collapseIcon: "-",
-};
-
+  ];
 
   return (
-    <div id='faqs' className='faqs'>
+    <div className="faqs" id='faqs'>
       <h3>Frequently Asked Questions</h3>
-      <Faq
-        data={data}
-        styles={styles}
-        config={config}
-      />
+      {data.map(({title, content}) => (
+         <Accordion title={title} content={content} />
+      ))}
       <h4>Still have questions?</h4>
       <p>If you cannot find answer to your question in our FAQ, you can always contact us. We will always be here to answer you!</p>
     </div>
