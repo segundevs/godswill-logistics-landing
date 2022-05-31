3f817e3788bc17367e0d2b19fecf65edd49757c7
segundevs <tushbash@gmail.com>
2022-05-31 03:51:16 -0700

    added footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/logo2.svg';
+import { IoLogoFacebook, IoLogoInstagram, IoLogoWhatsapp } from 'react-icons/io5';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className="footer_content">
+        <div className="logo" style={{color: 'white'}}> 
+        <img src={logo} alt="brand-logo"/>
+        <div>
+          <span>wills royalty</span>
+          <span>logistics</span>
+        </div>
+      </div>
+
+      <div className="footer_links">
+        <a href="#hero">Home</a>
+        <a href="#services">Services</a>
+        <a href="#faqs">FAQ</a>
+        <a href="#contact">Contact us</a>
+      </div>
+
+      <div className="footer_social-links">
+        <a href="https://www.facebook.com/wills-royalty-logistics-107889328590552" target="_blank">
+          <IoLogoFacebook className='icon'/>
+        </a>
+        <a href="https://www.instagram.com/willsroyaltylogistics" target="_blank">
+          <IoLogoInstagram className='icon'/>
+        </a>
+        <a href="https://wa.me/+2348077114630/?text=Welcome to Wills Royalty Logistics, how may we help you today?" target="_blank">
+          <IoLogoWhatsapp className='icon'/>
+        </a>
+      </div>
+      </div>
+
+      <p>Copyright Will's Royalty Logistics - {new Date().getFullYear()}</p>
+      
+    </footer>
   )
 }
 
